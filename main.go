38d9e64007172942c71d08149b875c90d51f3b96
Shaxzodbek <muxtorovshaxzodbek16@gmail.com>
2025-02-22 15:20:19 +0500

    Update main.go

--- main.go
+++ main.go
@@ -6,4 +6,10 @@ import "rsc.io/quote"
 
 func main() {
 	fmt.Println(quote.Go())
+	arrayName := [5]string{"item1", "item2", "item3", "itemN"}
+	fmt.Println(arrayName)
+}
+
+func bs(arrayName string, target int) int {
+	return target
 }
